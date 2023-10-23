5bf99fb0093ae59ed538aeda41683737459efc88
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-10-24 00:40:43 +0700

    cek accesicbility

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,13 +7,13 @@ export default function Footer() {
     <footer className="bg-slate-800 text-white p-6 flex gap-8 flex-col md:flex-row justify-center items-center">
       <h1>Powered by &copy; Muhammad Nur Maulana 2023 </h1>
       <div className="flex items-center gap-8 text-xl">
-        <Link href={"https://github.com/MuhammadNurMaulana"} target="_blank">
+        <Link href={"https://github.com/MuhammadNurMaulana"} target="_blank" aria-label="Link Menuju Github">
           <FaGithub />
         </Link>
-        <Link href={"https://wa.me/+6283137446693"} target="_blank">
+        <Link href={"https://wa.me/+6283137446693"} target="_blank" aria-label="Link Menuju Whatsapp">
           <FaWhatsapp />
         </Link>
-        <Link href={"https://www.instagram.com/ctizz4/"}>
+        <Link href={"https://www.instagram.com/ctizz4/"} target="_blank" aria-label="Link Menuju Instagram">
           <FaInstagram />
         </Link>
       </div>

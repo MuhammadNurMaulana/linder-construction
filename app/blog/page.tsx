697c9a0392dd9ff229edc1bfb8152febf35fe57c
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-10-24 23:50:28 +0700

    change contras bg

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,18 +10,18 @@ export default async function BlogPage() {
     <>
       <BlogHeader />
 
-      <section className="w-4/5 mx-auto my-12">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <section className="my-12">
+        <div className="bg-[#F7F7F7] w-4/5 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {blogs.data.length > 0 &&
             blogs.data.map((blog: any) => (
-              <div key={blog.id} className="relative pb-12 bg-[#F7F7F7] rounded">
+              <div key={blog.id} className="relative pb-12 bg-white rounded">
                 <Image src={blog.img} width={350} height={350} alt={blog.desc} className="w-full rounded h-[35vh] " />
 
                 <div className="p-4">
                   <p className="text-xs text-slate-400 font-semibold mb-2">{blog.date}</p>
                   <h2 className="line-clamp-5">{blog.desc}</h2>
 
-                  <Link href={`/blog/detail/${blog.id}`} aria-description="Read More" aria-label="read-more" className="absolute bottom-4 left-4 border-2 border-slate-800 hover:border-slate-900 py-2 px-4 ">
+                  <Link href={`/blog/detail/${blog.id}`} aria-label={blog.desc} className="absolute bottom-4 left-4 border-2 border-slate-800 hover:border-slate-900 py-2 px-4 rounded">
                     Read More...
                   </Link>
                 </div>

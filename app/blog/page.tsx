3d45cf2e5903ac2f73793ab1894d7a8657f0024f
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-10-24 21:16:50 +0700

    cek

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,28 +1,10 @@
 import BlogHeader from "@/components/Blogs/BlogHeader";
-import { GetData } from "@/services/blog";
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
-export default async function BlogPage() {
-  const blogs = await GetData(`${process.env.API_KEY_URL}/api/blog`);
+export default function BlogPage() {
   return (
     <>
       <BlogHeader />
-
-      <section className="w-4/5 mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {blogs.data.length > 0 &&
-            blogs.data.map((blog: { id: number; desc: string; date: string; img: string }) => (
-              <div key={blog.id}>
-                <Image src={blog.img} width={350} height={350} alt={blog.desc} className="w-full rounded h-[35vh]" />
-                <p>{blog.date}</p>
-                <h2 className="line-clamp-5">{blog.desc}</h2>
-                <Link href={`/blog/detail/${blog.id}`}>Read More</Link>
-              </div>
-            ))}
-        </div>
-      </section>
     </>
   );
 }

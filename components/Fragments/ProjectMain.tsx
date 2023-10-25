c3d66fb105e62b914ab9ef2a76b4d2a651218124
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-10-25 21:25:37 +0700

    cek

--- components/Fragments/ProjectMain.tsx
+++ components/Fragments/ProjectMain.tsx
@@ -11,7 +11,7 @@ interface Project {
 export default function ProjectMain({ src, alt, title, btnName }: Project) {
   return (
     <div>
-      <Image src={src} alt={alt} width={600} height={600} className="rounded-lg h-72 object-cover" />
+      <Image src={src} alt={alt} width={600} height={600} quality={100} className="w-full rounded-lg h-72 object-cover" />
       <div className="pt-12 pb-4 px-4">
         <h1 className="text-lg font-semibold">{title}</h1>
         <p className="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam atque aut, eveniet cupiditate autem praesentium.</p>

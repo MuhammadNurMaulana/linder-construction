6028e56f5cab30958e4c46bd615594650dd3dca3
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-10-25 18:01:27 +0700

    up

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,8 +3,6 @@ import { getData } from "@/services/blog";
 import React from "react";
 
 export default async function PortfolioPage() {
-  const projects = await getData(`${process.env.API_KEY_URL}/api/portofolio`);
-  console.log(projects);
   return (
     <>
       <section className="bg-home1">

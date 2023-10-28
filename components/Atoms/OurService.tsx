d306dd535f72f249d3ab86c551e8c60d6450c754
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-10-28 10:07:31 +0700

    cek

--- components/Atoms/OurService.tsx
+++ components/Atoms/OurService.tsx
@@ -10,7 +10,7 @@ interface OurServiceProps {
 export default function OurService({ src, alt, title }: OurServiceProps) {
   return (
     <div className="flex gap-4">
-      <Image src={src} width={70} height={70} alt={alt} className="w-[70px] h-[70px]" />
+      <Image src={src} width={100} height={100} quality={100} alt={alt} className="w-[70px] h-[70px]" />
       <div>
         <h2 className="text-xl font-semibold">{title}</h2>
         <p className="text-xs font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing.</p>

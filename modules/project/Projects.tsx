f65bac8a37dfc41a7060c0b28e9be222bf4a3650
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-11-01 13:19:09 +0700

    cek

--- modules/project/Projects.tsx
+++ modules/project/Projects.tsx
@@ -1,12 +1,12 @@
-// import PageProject from "@/common/elements/PageProject";
+import PageProject from "@/common/elements/PageProject";
 import TitlePage from "@/common/elements/TitlePage";
-// import { getData } from "@/common/service";
+import { getData } from "@/common/service";
 import Link from "next/link";
 import React from "react";
 
 export default async function Projects() {
-  //   const project = await getData(`${process.env.API_KEY_URL}/api/project`);
-  //   const projects = project.data;
+  const project = await getData(`${process.env.API_KEY_URL}/api/project`);
+  const projects = project.data;
   return (
     <section className="py-12">
       <div className="py-12">
@@ -14,12 +14,12 @@ export default async function Projects() {
 
         <div className="w-4/5 mx-auto mt-12">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {/* {projects.length > 0 &&
+            {projects.length > 0 &&
               projects.map((project: any) => (
                 <div key={project.id}>
                   <PageProject href={`/allprojects/details/${project.id}`} src={project.img} alt={`image of project ${project.id}`} title={project.title} btnName="VIEW PROJECT" />
                 </div>
-              ))} */}
+              ))}
           </div>
         </div>
       </div>

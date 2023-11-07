c7e07f319dd5ba483f9ff79642fae135fa303fa8
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-11-07 11:54:56 +0700

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import React from "react";
 
 export default function NotFound() {
@@ -6,9 +5,6 @@ export default function NotFound() {
     <div className="w-screen h-screen flex items-center justify-center bg-slate-950">
       <div className="flex flex-col items-center justify-center gap-8 box-border w-[80%]">
         <h1 className="text-3xl font-bold text-white">Pages Is Not Found Back to Home</h1>
-        <Link href={"/"} className="px-4 rounded bg-white text-slate-950 font-semibold hover:py-2 transition-all duration-700 hover:px-8">
-          Main Page
-        </Link>
       </div>
     </div>
   );
